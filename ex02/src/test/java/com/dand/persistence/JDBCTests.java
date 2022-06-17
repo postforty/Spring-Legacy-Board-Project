525cfd5df20c7b21d20e193f2399e7eebade2ac3
postforty <75767073+postforty@users.noreply.github.com>
2022-06-17 23:24:11 +0900

    [UPDATE] MySQL 연동 및 테스트

---
+++ ex02/src/test/java/com/dand/persistence/JDBCTests.java
@@ -0,0 +1,35 @@
+package com.dand.persistence;
+
+import static org.junit.Assert.fail;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import org.junit.Test;
+
+import lombok.extern.log4j.Log4j;
+
+@Log4j
+public class JDBCTests {
+
+	static {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public void testConnection() {
+
+		try (Connection con = DriverManager.getConnection("jdbc:log4jdbc:mariadb://localhost:3306/book_ex", "root",
+				"1234")) {
+
+			log.info(con);
+		} catch (Exception e) {
+			fail(e.getMessage());
+		}
+	}
+
+}
