113ee1f6c0238c665c08ac787bdddea98381bda8
postforty <75767073+postforty@users.noreply.github.com>
2022-06-19 00:40:12 +0900

    [UPDATE] Mapper XML 처리

--- ex02/src/main/java/com/dand/mapper/BoardMapper.java
+++ ex02/src/main/java/com/dand/mapper/BoardMapper.java
@@ -8,7 +8,7 @@ import com.dand.domain.BoardVO;
 
 public interface BoardMapper {
 	
-	@Select("select * from tbl_board where bno > 0")
+//	@Select("select * from tbl_board where bno > 0")
 	public List<BoardVO> getList();
 
 }
