45f506b4c9680dc151138d7e2ed8cd9e97c71f4e
postforty <75767073+postforty@users.noreply.github.com>
2022-06-20 23:26:14 +0900

    [UPDATE] CRUD 실습

--- ex02/src/main/java/com/dand/mapper/BoardMapper.java
+++ ex02/src/main/java/com/dand/mapper/BoardMapper.java
@@ -14,5 +14,9 @@ public interface BoardMapper {
 	public void insert(BoardVO board);
 	
 	public void insertSelectKey(BoardVO board);
+	
+	public BoardVO read(Long bno);
+	
+	public int delete(Long bno);
 
 }
--- ex02/src/test/java/com/dand/mapper/BoardMapperTests.java
+++ ex02/src/test/java/com/dand/mapper/BoardMapperTests.java
@@ -19,35 +19,50 @@ public class BoardMapperTests {
 	@Setter(onMethod_ = @Autowired)
 	private BoardMapper mapper;
 	
-	@Test
-	public void testGetList() {
-		mapper.getList().forEach(board -> log.info(board));
-	}
+//	@Test
+//	public void testGetList() {
+//		mapper.getList().forEach(board -> log.info(board));
+//	}
+//	
+//	@Test
+//	public void testInsert() {
+//		
+//		BoardVO board = new BoardVO();
+//		board.setTitle("새로 작성하는 글");
+//		board.setContent("새로 작성하는 내용");
+//		board.setWriter("newbie");
+//		
+//		mapper.insert(board);
+//		
+//		log.info(board);
+//	}
+//	
+//	@Test
+//	public void testInsertSelectKey() {
+//		
+//		BoardVO board = new BoardVO();
+//		board.setTitle("새로 작성하는 글 select key");
+//		board.setContent("새로 작성하는 내용 select key");
+//		board.setWriter("newbie");
+//		
+//		mapper.insertSelectKey(board);
+//		
+//		log.info(board);
+//	}
+	
+//	@Test
+//	public void testRead() {
+//		
+//		BoardVO board = mapper.read(6L);
+//		
+//		log.info(board);
+//	}
 	
 	@Test
-	public void testInsert() {
+	public void testDelete() {
 		
-		BoardVO board = new BoardVO();
-		board.setTitle("새로 작성하는 글");
-		board.setContent("새로 작성하는 내용");
-		board.setWriter("newbie");
-		
-		mapper.insert(board);
-		
-		log.info(board);
+		log.info("DELETE COUNT: " + mapper.delete(6L));
 	}
 	
-	@Test
-	public void testInsertSelectKey() {
-		
-		BoardVO board = new BoardVO();
-		board.setTitle("새로 작성하는 글 select key");
-		board.setContent("새로 작성하는 내용 select key");
-		board.setWriter("newbie");
-		
-		mapper.insertSelectKey(board);
-		
-		log.info(board);
-	}
 
 }
