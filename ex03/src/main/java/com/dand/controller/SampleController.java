5071a713ca423dbcdb093ae6ae0bad66ee658ab1
postforty <75767073+postforty@users.noreply.github.com>
2022-07-05 00:19:41 +0900

    [UPDATE] REST 방식으로 전환 - 객체의 반환

--- ex03/src/main/java/com/dand/controller/SampleController.java
+++ ex03/src/main/java/com/dand/controller/SampleController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.dand.domain.SampleVO;
+
 import lombok.extern.log4j.Log4j;
 
 @RestController
@@ -12,10 +14,17 @@ import lombok.extern.log4j.Log4j;
 @Log4j
 public class SampleController {
 
-	@GetMapping(value="/getText", produces="text/plain; charset=UTF-8")
+	@GetMapping(value = "/getText", produces="text/plain; charset=UTF-8")
 	public String getText() {
 		log.info("MIME TYPE: " + MediaType.TEXT_PLAIN_VALUE);
 		
 		return "안녕하세요";
+	}
+	
+//	@GetMapping(value = "/getSample", produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+	@GetMapping(value = "/getSample")
+	public SampleVO getSample() {
+		
+		return new SampleVO(112, "스타", "로드");
 	}
 }
